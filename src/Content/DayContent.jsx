0c4c0958caaaa16679dc6138d346a9d0a9554e46
rmartinezparra <79370594+rmartinezparra@users.noreply.github.com>
2023-08-11 19:52:51 +0200

    Increased text size for day

--- src/Content/DayContent.jsx
+++ src/Content/DayContent.jsx
@@ -4,7 +4,7 @@ function DayContent({infoDay}) {
   return (
     <section className="w-full min-h-min xl:h-screen">
       <div className="pt-20 xl:pt-28 text-center text-zinc-200 flex flex-col antialiased">
-        <div className="flex-auto font-bodoni font-bold text-4xl xl:text-8xl">{infoDay.day}</div>
+        <div className="flex-auto font-bodoni font-bold text-5xl xl:text-8xl">{infoDay.day}</div>
         <div className="flex-auto font-brixton font-thin text-2xl xl:text-3xl uppercase tracking-widest underline decoration-solid decoration-1">{infoDay.dayWeek}</div>
         <div className="flex-auto mt-3">
           {
